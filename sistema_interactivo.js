93fb2c29dc7d74870d909fde101d4d03df767533
joinercantillo <jcantill55@gmail.com>
2026-05-04 17:38:43 -0500

    update hu semana 1

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,2 +1,17 @@
-const nombre = prompt('Dime cual es tu nombre: ')
-alert('ya se que tu nombre es ')
+const nombre = prompt('Dime cual es tu nombre: ');
+
+let edad = prompt('Dime cual es tu edad: ');
+
+
+if (!isNaN(edad) && edad.trim() !== "") {
+    console.log("Es un número válido");
+} else {
+    console.error("Error:por favor ingresa una edad valiada con numeros");
+}
+
+if (edad < 18) {
+    alert(`Hola ${nombre}, eres menor de edad, !Sigue aprendiendo y disfrutando del código!`);
+} else {
+    alert(`Hola ${nombre}, eres mayor de edad, Prepárate para grandes oportunidades en el mundo de la programación!`);
+}
+
